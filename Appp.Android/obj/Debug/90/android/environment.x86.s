b4efd5b9a9403263a0da0a8f8639177390f9dac5
Дмитрий Козлов <lebedev_mityusha@outlook.com>
2020-07-15 14:18:59 +0300

    Add TabBar

--- Appp.Android/obj/Debug/90/android/environment.x86.s
+++ Appp.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"5a1658c6-9730-4033-b2d5-99bc3ccf42ef"
+	.asciz	"33ea6e0b-3fb6-40ba-bc93-c320f256025f"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- Appp/App.xaml.cs
+++ Appp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Appp
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
