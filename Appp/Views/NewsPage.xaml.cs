85c42b5f1d8b98dc94a8942a282268a205aa70a1
Дмитрий Козлов <lebedev_mityusha@outlook.com>
2020-07-17 15:06:01 +0300

    Get request

--- Appp/Views/NewsPage.xaml.cs
+++ Appp/Views/NewsPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Appp.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +19,20 @@ namespace Appp.Views
         public NewsPage()
         {
             InitializeComponent();
+            GetData();
         }
+
+        private async void GetData()
+        {
+            var access_token = Application.Current.Properties["token"].ToString();
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", access_token);
+            var result = await client.GetAsync("http://cabinets.itmit-studio.ru/api/news");
+            var content = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(access_token);
+            Console.WriteLine(content);
+
+        }
+
     }
 }
